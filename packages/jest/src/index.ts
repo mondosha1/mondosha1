eadf8e67cadbbed0d5b1ea53eed0a6a692041b59
Yannick Galatol <yannick.galatol@emoteev.io>
2020-07-16 10:41:38 +0200

    Merge pull request #2579 from Elium/feat/google-ads/implement-affinities-targeting
    GitOrigin-RevId: 3d8a2bc1ef041e3d3c0fc5ac09b200e8917587b2

--- packages/jest/src/index.ts
+++ packages/jest/src/index.ts
@@ -2,5 +2,6 @@
 import 'jest-chain'
 import 'jest-extended'
 import { toBeActionObservable } from './action-observable.matcher'
+import { toJsonEqual } from './json-equal.matcher'
 
-expect.extend({ toBeActionObservable })
+expect.extend({ toBeActionObservable, toJsonEqual })

9065535769152f14af67190316e6965c6e9d050f
Yannick Galatol <yannick.galatol@emoteev.io>
2021-01-13 17:01:28 +0100

    Merge pull request #2725 from Elium/develop
    GitOrigin-RevId: 92bbaf05d514cecee03462acea6836d1381cfdb9

---
+++ tools/scripts/run-tasks.js
@@ -0,0 +1,16 @@
+// tslint:disable
+// eslint:disable
+const execSync = require('child_process').execSync
+
+const tasks = process.argv[2]
+const [target, projects] = tasks.split(':')
+
+console.log(`Projects to ${target}: ${projects}.`)
+const remainingArgs = process.argv
+  .slice(3)
+  .map(a => `"${a}"`)
+  .join(' ')
+execSync(
+  `node --max-old-space-size=8000 ./node_modules/@nrwl/cli/bin/nx.js run-many --target=${target} --projects=${projects} --parallel ${remainingArgs}`,
+  { stdio: 'inherit' }
+)

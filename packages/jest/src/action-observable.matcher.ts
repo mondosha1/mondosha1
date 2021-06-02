3640cfe89581779f507079b4a5fedb23ffbdcb4b
Yannick Galatol <yannick.galatol@emoteev.io>
2021-06-02 09:35:13 +0200

    Merge pull request #2961 from Elium/core/angular/migrate-to-nx-and-angular-12
    GitOrigin-RevId: 56ba87697eec27a4bb860fc8e06c1f0e6b39a200

--- packages/jest/src/action-observable.matcher.ts
+++ packages/jest/src/action-observable.matcher.ts
@@ -6,24 +6,30 @@ import { Notification } from 'rxjs'
 import { TestMessage } from 'rxjs/internal/testing/TestMessage'
 import { TestScheduler } from 'rxjs/testing'
 
+export function actionObservable<T>(expected$: TestObservable) {
+  return (received$: TestObservable) => {
+    const results: TestMessage[] = []
+    const scheduler = getTestScheduler()
+
+    scheduler.schedule(() => {
+      received$.subscribe(
+        (value: any) =>
+          results.push({ frame: scheduler.frame, notification: Notification.createNext(omit('uuid')(value)) }),
+        (err: any) => results.push({ frame: scheduler.frame, notification: Notification.createError(err) }),
+        () => results.push({ frame: scheduler.frame, notification: Notification.createComplete() })
+      )
+    })
+    scheduler.flush()
+
+    const valuesWithoutUuid = mapValues(omit('uuid'))(expected$.values)
+    const expected = TestScheduler.parseMarbles(expected$.marbles, valuesWithoutUuid, expected$.error, true)
+
+    // @todo:: add a way to display debug
+    //         Warning : JSON.stringify can clean keys with values undefined !
+    return equals(expected)(results)
+  }
+}
+
 export function toBeActionObservable<T>(received$: TestObservable, expected$: TestObservable) {
-  const results: TestMessage[] = []
-  const scheduler = getTestScheduler()
-
-  scheduler.schedule(() => {
-    received$.subscribe(
-      (value: any) =>
-        results.push({ frame: scheduler.frame, notification: Notification.createNext(omit('uuid')(value)) }),
-      (err: any) => results.push({ frame: scheduler.frame, notification: Notification.createError(err) }),
-      () => results.push({ frame: scheduler.frame, notification: Notification.createComplete() })
-    )
-  })
-  scheduler.flush()
-
-  const valuesWithoutUuid = mapValues(omit('uuid'))(expected$.values)
-  const expected = TestScheduler.parseMarbles(expected$.marbles, valuesWithoutUuid, expected$.error, true)
-
-  // @todo:: add a way to display debug
-  //         Warning : JSON.stringify can clean keys with values undefined !
-  return { pass: equals(expected)(results), message: () => `Action and resultAction aren't equals` }
+  return { pass: actionObservable(expected$)(received$), message: () => `Action and resultAction aren't equals` }
 }
--- packages/jest/src/index.ts
+++ packages/jest/src/index.ts
@@ -5,3 +5,6 @@ import { toBeActionObservable } from './action-observable.matcher'
 import { toJsonEqual } from './json-equal.matcher'
 
 expect.extend({ toBeActionObservable, toJsonEqual })
+
+export { actionObservable } from './action-observable.matcher'
+export { jsonEqual } from './json-equal.matcher'

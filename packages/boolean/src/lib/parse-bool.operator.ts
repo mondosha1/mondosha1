8d258af34469da38433b4fcc13bf6c23a2045fe3
Yannick Galatol <yannick.galatol@emoteev.io>
2022-06-13 16:57:09 +0200

    Merge pull request #3392 from Elium/feat/auth/allow-to-set-cookie-server-side
    GitOrigin-RevId: 7ef8b5c8bbe4922b8cf20e814b0adf64b76e47c1

--- packages/boolean/src/lib/parse-bool.operator.ts
+++ packages/boolean/src/lib/parse-bool.operator.ts
@@ -1,13 +1,13 @@
 import { Nullable } from '@mondosha1/nullable'
 
 export function parseBool(value: boolean | BooleanNumber | BooleanAsString | BooleanNumberAsString): Nullable<boolean> {
-  if (value === true || value === 1 || value === 'true' || value === '1') {
+  if (value === true || value === 1 || value === 'true' || value === 'TRUE' || value === '1') {
     return true
-  } else if (value === false || value === 0 || value === 'false' || value === '0') {
+  } else if (value === false || value === 0 || value === 'false' || value === 'FALSE' || value === '0') {
     return false
   }
 }
 
-export type BooleanAsString = 'true' | 'false'
+export type BooleanAsString = 'true' | 'false' | 'TRUE' | 'FALSE'
 export type BooleanNumber = 1 | 0
 export type BooleanNumberAsString = '1' | '0'

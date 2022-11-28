98a8b7cc5be01c0394794e4cbc9e466e8886ecbb
Y.Alain <51077705+Feifei75@users.noreply.github.com>
2022-11-28 19:09:49 +0100

    Merge pull request #3715 from Elium/feat/mapping/overall-code-improvement
    feat(mapping): overall code improvement, should be fail proof
    
    GitOrigin-RevId: 7c6bf7bfce795c4db78e7ac736d06439ec743f2b

--- packages/nullable/src/lib/fold.operator.ts
+++ packages/nullable/src/lib/fold.operator.ts
@@ -5,7 +5,7 @@ export function fold<RightInput, LeftOutput, RightOutput>(
   left: ((v: Nil) => LeftOutput) | LeftOutput,
   right: ((v: NonNullable<RightInput>) => RightOutput) | RightOutput = identity
 ): (value: NonNullable<RightInput> | Nil) => LeftOutput | RightOutput {
-  return foldOn<Nil, RightInput, LeftOutput, RightOutput>(isNil, left, right)
+  return foldOn<Nil, NonNullable<RightInput>, LeftOutput, RightOutput>(isNil, left, right)
 }
 
 export function foldLeft<RightInput, LeftOutput>(
@@ -15,21 +15,21 @@ export function foldLeft<RightInput, LeftOutput>(
 }
 
 export function foldRight<RightInput, RightOutput>(
-  right: ((v: RightInput) => RightOutput) | RightOutput
-): (value: RightInput | Nil) => RightOutput | Nil {
-  return foldOn<Nil, RightInput, Nil, RightOutput>(isNil, identity, right)
+  right: ((v: NonNullable<RightInput>) => RightOutput) | RightOutput
+): (value: NonNullable<RightInput> | Nil) => RightOutput | Nil {
+  return foldOn<Nil, NonNullable<RightInput>, Nil, RightOutput>(isNil, identity, right)
 }
 
 export function foldOn<LeftInput, RightInput = LeftInput, LeftOutput = LeftInput, RightOutput = RightInput>(
   condition: ((v: LeftInput | RightInput) => v is LeftInput) | ((v: LeftInput | RightInput) => boolean) | boolean,
   left: ((v: LeftInput) => LeftOutput) | LeftOutput,
-  right: ((v: NonNullable<RightInput>) => RightOutput) | RightOutput = identity
+  right: ((v: RightInput) => RightOutput) | RightOutput = identity
 ): (value: LeftInput | RightInput) => LeftOutput | RightOutput {
   const leftFn = isFunction(left) ? left : constant(left)
   const rightFn = isFunction(right) ? right : constant(right)
   return (value: LeftInput | RightInput) => {
     const condFn = isFunction(condition) ? condition(value) : condition
-    return condFn ? leftFn(value as LeftInput) : rightFn(value as NonNullable<RightInput>)
+    return condFn ? leftFn(value as LeftInput) : rightFn(value as RightInput)
   }
 }
 
